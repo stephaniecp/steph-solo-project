2694374a7aa64e0d25649f335151eb32053b599e
Stephanie CP <stephers.cp@gmail.com>
2022-12-11 12:16:01 -0800

    Refractored. All tests working

--- testsMain.test.ts
+++ testsMain.test.ts
@@ -10,42 +10,38 @@ beforeAll(async () => {
   await pageObject.showMouseMovement()
 })
 
-describe("#1 Verify navbar elements are all present", () => {
+describe("#1 Navbar elements + store and logo links", () => {
     test("1.0 Can navigate to the Van's store and return to the home page", async () => {
         await pageObject.clickToAcessStore()
         await pageObject.getElement(pageObject.byCartBtnCss) // Verifying element specific to the Store
         await pageObject.click(pageObject.byVansLogoNavBar) // Returning to the home page
-        console.log("1.0 Done - Navigated to the Van's store and return to the home page")
+        console.log("Navigated to the Van's store and returned to the home page")
     })
-    test("1.1 Can locate the main nav bar elements", async() => { 
+    test("1.1 Can locate the nav bar menu items", async() => { 
+        // Main
         await pageObject.verifyElementsExist(pageObject.getAllMainNavLocators())
-        console.log("1.1 Done - Located the main 8 navbar elements")
-    })
-    test("1.2 Can locate the 9 Aircraft sub menu items", async() => { 
+        console.log("Located the main 8 navbar elements")
+        // Aircraft
         console.log("Back to main page")
         await pageObject.click(pageObject.byAircraftNavCss)
         await pageObject.verifyElementsExist(pageObject.getAllAircraftNavLocators())
-        console.log("1.2 Done - Located all sub menu items below Aircraft")
-    })
-    test("1.3 Can locate the 8 Learn sub menu items", async() => {  
+        console.log("Located all sub menu items below Aircraft")
+        // Learn
         await pageObject.click(pageObject.byLearnNavCss)
         await pageObject.verifyElementsExist(pageObject.getAllLearnNavLocators())
-        console.log("1.3 Done - Located all sub menu items below Learn")
-    })
-    test("1.4 Can locate the 7 Build sub menu items", async() => {  
+        console.log("Located all sub menu items below Learn")
+        // Build
         await pageObject.click(pageObject.byBuildNavCss)
         await pageObject.verifyElementsExist(pageObject.getAllBuildNavLocators())
-        console.log("1.4 Done - Located all sub menu items below Build")
-    })
-    test("1.5 Can locate the 6 Support sub menu items", async() => {  
+        console.log("Located all sub menu items below Build")
+        // Support
         await pageObject.click(pageObject.bySupportNavCss)
         await pageObject.verifyElementsExist(pageObject.getAllSupportNavLocators())
-        console.log("1.5 Done - Located all sub menu items below Support")
-    })
-    test("1.6 Can locate the 6 Communauty sub menu items", async() => {  
+        console.log("Located all sub menu items below Support")
+        // Community
         await pageObject.click(pageObject.byCommunityNavCss)
         await pageObject.verifyElementsExist(pageObject.getAllCommunityNavLocators())
-        console.log("1.6 Done - Located all sub menu items below Community")
+        console.log("Located all sub menu items below Community")
     })
 })
 
